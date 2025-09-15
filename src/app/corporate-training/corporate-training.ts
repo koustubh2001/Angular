988dd0f439cc2e40f123f4e2e9a4f79a80794fe1
koustubh <koustubhrayamane2001@gmail.com>
2025-09-15 11:27:13 +0530

    first commit

---
+++ src/app/corporate-training/corporate-training.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-corporate-training',
+  imports: [CommonModule],
+  templateUrl: './corporate-training.html',
+  styleUrl: './corporate-training.scss'
+})
+export class CorporateTraining {
+
+  
+   
+}
+
+
+
+
